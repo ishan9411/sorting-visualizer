a5edbc76712c61d5d02a25197c513089c983a93b
isha9411 <ishan9411@gmail.com>
2025-07-18 17:31:54 +0530

    added Selection Sort Algorithm

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ function generateArray(num = 40){
         container.appendChild(bar);
     }
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//Selection Sort
+async function selectionSort(num = 40){
+    const bars = document.getElementsByClassName("bar");
+    for(let i = 0; i<num-1; i++){
+        let minIndex = i;
+        bars[minIndex].style.backgroundColor = "green";
+        for(let j = i+1; j<num; j++){
+            bars[j].style.backgroundColor = "red";
+            await sleep(20);
+            if(arr[j] < arr[minIndex]){
+                bars[minIndex].style.backgroundColor = "#007bff";
+                minIndex = j;
+                bars[minIndex].style.backgroundColor = "green";
+            }else{
+                bars[j].style.backgroundColor = "#007bff";
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        await sleep(50);
+        bars[i].style.height = `${arr[i]}px`;
+        bars[minIndex].style.height = `${arr[minIndex]}px`;
+        await sleep(30);
+        bars[minIndex].style.backgroundColor = "#007bff";
+    }
+}
